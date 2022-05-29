36d83826274ab1deaaa8d6f67ca2b3dab122bab9
Maryam Fazlali <fazlali1202@gmail.com>
2022-05-29 18:56:50 +0100

    Implemented bookmarkController and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'lists#index'
-  resources :lists, except: %i[index destroy edit update]
+  resources :lists, except: %i[index destroy edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+  delete 'bookmarks/:id', to: 'bookmarks#destroy'
 end
